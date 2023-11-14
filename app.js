f731a87bb52a0c82a80a753c792b2fc52bca8ed3
Вячеслав <goldensoldat@gmail.com>
2023-11-14 21:04:49 +0200

    Add Swagger UI Express

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 
@@ -16,6 +18,8 @@ app.use(express.static("public"));
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
